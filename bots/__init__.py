7ef362de5088d0bfac00cff12027f8884dca79a3
grandatlant <grandatlant@gmail.com>
2025-09-11 18:19:35 +0300

    uv script metadata for main.py, new ruff settings file for auto code formatting with ruff or uv.

--- bots/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S python3 -O
-# -*- coding = utf-8 -*-
-"""bots package.
-"""
-
-from .base import *
--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env -S python3 -O
 # -*- coding = utf-8 -*-
-"""bridgebot main script.
-"""
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "python-dotenv",
+#   "discord.py",
+#   "python-telegram-bot",
+# ]
+# ///
+"""redirector.bot main script."""
 
 import sys
+import logging
 import asyncio
 
-# pip install discord.py python-telegram-bot
 import telegram
 import discord
 from discord.ext.commands import Bot as DiscordBot
-
-# pip install python-dotenv
 from dotenv import dotenv_values
-_env = dotenv_values()
 
-import logging
-logging.basicConfig(
-    level = logging.DEBUG if __debug__ else logging.INFO,
-    stream = sys.stdout,
-    style = '{',
-    format = '{levelname}:{name}:{message}',
-)
 log = logging.getLogger(__name__)
+
+_env = dotenv_values()
 
 DISCORD_TOKEN = _env.get('DISCORD_TOKEN') or 'No-Token'
 DISCORD_CHANNEL_IDS = [
-    int(i)
-    for i in (_env.get('DISCORD_CHANNEL_IDS') or '').split(',')
-    if i
+    int(i) for i in (_env.get('DISCORD_CHANNEL_IDS') or '').split(',') if i
 ]
 # Additional filters. Disabled if (False)
 ALLOWED_MENTION_IDS = []
@@ -45,7 +42,7 @@ tg_bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 async def transfer_message(message: str):
-    #await tg_bot.send_message(TELEGRAM_CHAT_ID, message)
+    # await tg_bot.send_message(TELEGRAM_CHAT_ID, message)
     #'''
     await asyncio.get_running_loop().run_in_executor(
         None,
@@ -63,16 +60,13 @@ async def on_ready():
 
 @dc_bot.event
 async def on_message(message: discord.Message):
-
     if message.author == dc_bot.user or message.guild is None:
         # Ignore made by bot and private messages
         return
-    
+
     if message.channel.id in DISCORD_CHANNEL_IDS:
-        
         mention_ok = not ALLOWED_MENTION_IDS or any(
-            user.id in ALLOWED_MENTION_IDS
-            for user in message.mentions
+            user.id in ALLOWED_MENTION_IDS for user in message.mentions
         )
         author_ok = not ALLOWED_AUTHOR_IDS or (
             message.author.id in ALLOWED_AUTHOR_IDS
@@ -80,18 +74,24 @@ async def on_message(message: discord.Message):
 
         if mention_ok or author_ok:
             guild = message.guild.name
-            channel = message.channel.name # pyright: ignore[reportAttributeAccessIssue]
+            channel = message.channel.name  # pyright: ignore[reportAttributeAccessIssue]
             author = message.author.display_name
             content = message.content
             text = f'{guild}.{channel}: {author}: {content}'
             log.info('Got message: %s', text)
-            #await transfer_message(text)
+            # await transfer_message(text)
 
     await dc_bot.process_commands(message)
 
 
 ##  MAIN ENTRY POINT
 def main(args=None):
+    logging.basicConfig(
+        level=logging.DEBUG if __debug__ else logging.INFO,
+        stream=sys.stdout,
+        format='%(levelname)s:%(name)s:%(message)s',
+    )
+    log.info('Running %s on %s', sys.version, sys.platform)
     token = DISCORD_TOKEN
     if args and len(args) == 2:
         token = args[1]

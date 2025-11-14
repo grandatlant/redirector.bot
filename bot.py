b7970bc8c57c928db9a4a566a73ccdc742c86c8a
grandatlant <grandatlant@gmail.com>
2025-11-14 05:50:03 +0200

    Moved Discord bot code to separate module for use as singleton

---
+++ bot.py
@@ -0,0 +1,205 @@
+r"""Redirector.Bot control module.
+
+Contains single Discord Bot instance with necessary interface methods and data.
+"""
+
+from __future__ import annotations
+
+import os
+import logging
+import asyncio
+import collections
+
+from typing import (
+    Any,
+    TypeVar,
+    Callable,
+    Iterable,
+    Coroutine,
+)
+
+import discord
+from discord.ext.commands import Bot as DiscordBot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+log: logging.Logger = logging.getLogger(__name__)
+log.setLevel(os.getenv('LOG_LEVEL') or logging.WARNING)
+
+T = TypeVar('T')
+Coro = Coroutine[Any, Any, T]
+CoroT = TypeVar('CoroT', bound=Callable[..., Coro[Any]])
+ReceiverT = TypeVar('ReceiverT', bound=Callable[[str], Any])
+
+DISCORD_CHANNEL_IDS: list[int] = [
+    int(i.strip())
+    for i in (os.getenv('DISCORD_CHANNEL_IDS') or '').split(',')
+    if i.strip()
+]
+
+# Additional filters. Disabled if empty
+DISCORD_ALLOWED_AUTHOR_IDS: list[int] = [
+    int(i.strip())
+    for i in (os.getenv('DISCORD_ALLOWED_AUTHOR_IDS') or '').split(',')
+    if i.strip()
+]
+DISCORD_ALLOWED_MENTION_IDS: list[int] = [
+    int(i.strip())
+    for i in (os.getenv('DISCORD_ALLOWED_MENTION_IDS') or '').split(',')
+    if i.strip()
+]
+
+
+# Global instance setup
+
+class Bot(DiscordBot):
+    pass
+
+instance: Bot | None = None
+
+defaults: dict[str, Any] = {
+    'command_prefix': os.getenv('DISCORD_COMMAND_PREFIX') or '!',
+    'intents': (
+        discord.Intents(int(os.getenv('DISCORD_INTENTS') or '0', base=0))
+        or discord.Intents.default()
+    ),
+}
+defaults['intents'].message_content = True
+
+
+# Bot events stored here to apply in init() call.
+events: list[CoroT] = []
+
+
+def event(coro: CoroT) -> CoroT:
+    """Decorator for registering new bot event handler coroutine."""
+    events.append(coro)
+    if instance is not None:
+        # register it for instance
+        return instance.event(coro)
+    return coro
+
+
+def register_events(
+    extend_events: Iterable[CoroT] | None = None,
+) -> list[CoroT]:
+    """Register global events, extended with extend_register if not None.
+
+    Return value: all events list.
+    """
+    events.extend(extend_events or [])
+    for event in events:
+        instance.event(event)
+    return events
+
+
+# Bot message receivers stored here to iterate in transfer_message() call.
+receivers: list[ReceiverT] = []
+
+
+def receiver(func: ReceiverT) -> ReceiverT:
+    """Decorator for registering new text message receiver callable."""
+    receivers.append(func)
+    return func
+
+
+def add_receivers(
+    extend_receivers: Iterable[ReceiverT] | None = None,
+) -> list[ReceiverT]:
+    """Add more text message receivers for bot.
+
+    Return value: all receivers list.
+    """
+    receivers.extend(extend_receivers or [])
+    return receivers
+
+
+@event
+async def on_ready() -> None:
+    log.debug('instance.on_ready() call.')
+    log.info('Logged in as %s.', instance.user)
+
+
+@event
+async def on_message(message: discord.Message) -> None:
+    log.debug('instance.on_message(%r) call.', message)
+    if message.author == instance.user or message.guild is None:
+        # Ignore made by bot and private messages
+        return
+
+    async with asyncio.TaskGroup() as tasks:
+        tasks.create_task(instance.process_commands(message))
+
+        if should_transfer_message(message):
+            # Form and transfer message in new async task
+            text = get_message_text(message)
+            log.info('--> Text for transfer: %r. <--', text)
+            tasks.create_task(transfer_text_message(text))
+        else:
+            log.debug('--> Message ignored: "%s". <--', message)
+
+
+def should_transfer_message(message: discord.Message) -> bool:
+    if message.channel.id in DISCORD_CHANNEL_IDS:
+        author_ok: bool = not DISCORD_ALLOWED_AUTHOR_IDS or (
+            message.author.id in DISCORD_ALLOWED_AUTHOR_IDS
+        )
+        mention_ok: bool = not DISCORD_ALLOWED_MENTION_IDS or any(
+            user.id in DISCORD_ALLOWED_MENTION_IDS for user in message.mentions
+        )
+        if author_ok or mention_ok:
+            return True
+    return False
+
+
+def get_message_text(message: discord.Message) -> str:
+    guild: str = message.guild.name
+    channel: str = getattr(message.channel, 'name', 'Unknown channel')
+
+    author: str = message.author.display_name
+    content: str = message.clean_content
+
+    return f'{guild}.{channel}: {author}: {content}'
+
+
+async def transfer_text_message(text: str) -> None:
+    async with asyncio.TaskGroup() as tasks:
+        for receiver in receivers:
+            if asyncio.iscoroutinefunction(receiver):
+                tasks.create_task(receiver(text))
+            else:
+                tasks.create_task(asyncio.to_thread(receiver, text))
+
+
+# Redirect unknown requests to instance object.
+def __getattr__(name, *args, **kwargs):
+    return getattr(instance, name, *args, **kwargs)
+
+
+def init(*args, **kwargs) -> Bot:
+    """Recreate bot instance.
+
+    Return value: Bot instance itself.
+    """
+    global instance
+    instance = Bot(*args, **collections.ChainMap(kwargs, defaults))
+    register_events()
+    return instance
+
+
+def main(*args, **kwargs) -> None:
+    instance.run(
+        token=os.getenv('DISCORD_TOKEN') or 'DISCORD_TOKEN',
+        *args,
+        **kwargs,
+    )
+
+
+if instance is None:
+    # Init with default values before possible use.
+    init()
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -15,111 +15,38 @@ from __future__ import annotations
 import os
 import sys
 import logging
-import asyncio
-
-from typing import (
-    Optional,
-    Union,
-    List,
-)
 
 import telegram
-import discord
-from discord.ext.commands import Bot as DiscordBot
 from dotenv import load_dotenv
+
+import bot
 
 load_dotenv()
 
-LOG_LEVEL: Union[str, int] = os.getenv('LOG_LEVEL') or (
+
+LOG_LEVEL: str | int = os.getenv('LOG_LEVEL') or (
     logging.DEBUG if __debug__ else logging.INFO
 )
 LOG_FORMAT: str = (
     os.getenv('LOG_FORMAT') or '%(levelname)s:%(name)s:%(message)s'
 )
-
 log: logging.Logger = logging.getLogger(__name__)
 log.setLevel(LOG_LEVEL)
 
 DISCORD_TOKEN: str = os.getenv('DISCORD_TOKEN') or 'No-Token'
-DISCORD_COMMAND_PREFIX: str = os.getenv('DISCORD_COMMAND_PREFIX') or '!'
-DISCORD_CHANNEL_IDS: List[int] = [
-    int(i.strip())
-    for i in (os.getenv('DISCORD_CHANNEL_IDS') or '').split(',')
-    if i.strip()
-]
-# Additional filters. Disabled if empty
-DISCORD_ALLOWED_AUTHOR_IDS: List[int] = [
-    int(i.strip())
-    for i in (os.getenv('DISCORD_ALLOWED_AUTHOR_IDS') or '').split(',')
-    if i.strip()
-]
-DISCORD_ALLOWED_MENTION_IDS: List[int] = [
-    int(i.strip())
-    for i in (os.getenv('DISCORD_ALLOWED_MENTION_IDS') or '').split(',')
-    if i.strip()
-]
-
 TELEGRAM_TOKEN: str = os.getenv('TELEGRAM_TOKEN') or 'No-Token'
 TELEGRAM_CHAT_ID: int = int(os.getenv('TELEGRAM_CHAT_ID') or 0)
 
-intents: discord.Intents = discord.Intents.default()
-intents.message_content = True
-dc_bot: DiscordBot = DiscordBot(
-    command_prefix=DISCORD_COMMAND_PREFIX,
-    intents=intents,
-)
 
-
-async def transfer_message(message: str) -> None:
-    log.debug('%s.transfer_message(%r) call.', __name__, message)
+@bot.receiver
+async def telegram_send(message: str) -> None:
+    log.debug('%s.telegram_send(%r) call.', __name__, message)
     async with telegram.Bot(token=TELEGRAM_TOKEN) as tg_bot:
         await tg_bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
-@dc_bot.event
-async def on_ready() -> None:
-    # log.debug('dc_bot.on_ready() call.')
-    log.info('Logged in as %s.', dc_bot.user)
-
-
-@dc_bot.event
-async def on_message(message: discord.Message) -> None:
-    log.debug('dc_bot.on_message(%r) call.', message)
-    if message.author == dc_bot.user or message.guild is None:
-        # Ignore made by bot and private messages
-        return
-
-    tasks: List[asyncio.Future] = [
-        asyncio.create_task(dc_bot.process_commands(message))
-    ]
-
-    if message.channel.id in DISCORD_CHANNEL_IDS:
-        author_ok: bool = not DISCORD_ALLOWED_AUTHOR_IDS or (
-            message.author.id in DISCORD_ALLOWED_AUTHOR_IDS
-        )
-        mention_ok: bool = not DISCORD_ALLOWED_MENTION_IDS or any(
-            user.id in DISCORD_ALLOWED_MENTION_IDS for user in message.mentions
-        )
-
-        if author_ok and mention_ok:
-            # Form and transfer message in new async task
-            guild: str = message.guild.name
-            channel: str = getattr(message.channel, 'name', 'Unknown channel')
-
-            author: str = message.author.display_name
-            content: str = message.clean_content
-
-            text: str = f'{guild}.{channel}: {author}: {content}'
-
-            log.info('--> Text for transfer: %r.', text)
-
-            tasks.append(asyncio.create_task(transfer_message(text)))
-
-    await asyncio.gather(*tasks)
-
-
 ##  MAIN ENTRY POINT
-def main(args: Optional[List[str]] = None) -> None:
+def main(args: list[str] | None = None) -> None:
     logging.basicConfig(
         level=LOG_LEVEL,
         stream=sys.stdout,
@@ -130,7 +57,7 @@ def main(args: Optional[List[str]] = None) -> None:
     if args is None:
         args = sys.argv[1:]
     token: str = args[0] if args else DISCORD_TOKEN
-    dc_bot.run(token)
+    bot.run(token)
 
 
 if __name__ == '__main__':
